8e952a5cb7fbe99d60f490b923d52677355f0f77
Kyle Schreiber <kyle@80x24.net>
2022-07-21 23:50:16 -0700

    Use prebuild binary in formula

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -1,21 +1,21 @@
 class KestrelCli < Formula
   desc "File encryption done right"
   homepage "https://getkestrel.com"
-  url "https://storage.googleapis.com/kestrel-assets/kestrel-0.10.0.tar.gz"
-  sha256 "78756bf6de69b92174d5e003084bb0b8e3f495d794ad86c12f2b6be8c82a961d"
+  url "https://storage.googleapis.com/kestrel-assets/kestrel-macos-v0.10.0-all.tar.gz"
+  sha256 "8e638e46c7ee24f7f45919a400dd91cea4608e8e1a1a0d4be8a250fe0234cf0e"
   license "BSD-3-Clause"
-
-  bottle do
-    root_url "https://storage.googleapis.com/kestrel-assets"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "481adff64025cec409a1d2010ca399f19942763e40413cb299d18bb8098cff21"
-  end
-
-  depends_on "rust" => :build
+  version "0.10.0"
 
   def install
-    system "cargo", "build", "--release"
-    out_dir = Dir["target/release"].first
-    bin.install "#{out_dir}/kestrel"
+    # Determine which binary to install based on the current architecture
+    arch = `arch`.strip
+    if arch == "x86_64"
+      bin.install "bin/amd64/kestrel"
+    elsif arch == "arm64"
+      bin.install "bin/arm64/kestrel"
+    else
+      raise "Could not find valid architecture"
+    end
     man1.install "docs/man/kestrel.1"
     bash_completion.install "completion/kestrel.bash-completion"
   end
