60a0924341bc038907eecc0e43c2960c1b6754be
Kyle Schreiber <kyle@80x24.net>
2022-07-21 22:10:41 -0700

    Add bottle download

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -5,6 +5,11 @@ class KestrelCli < Formula
   sha256 "78756bf6de69b92174d5e003084bb0b8e3f495d794ad86c12f2b6be8c82a961d"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://storage.googleapis.com/kestrel-assets/"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "481adff64025cec409a1d2010ca399f19942763e40413cb299d18bb8098cff21"
+  end
+
   depends_on "rust" => :build
 
   def install
