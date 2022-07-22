e716ba3562d6e458c9140cf8d292b1f6baf04ff1
Kyle Schreiber <kyle@80x24.net>
2022-07-21 21:02:21 -0700

    Initial Commit

---
+++ Formula/kestrel-cli.rb
@@ -0,0 +1,19 @@
+class KestrelCli < Formula
+  desc "File encryption done right"
+  homepage "https://getkestrel.com"
+  url "https://storage.googleapis.com/kestrel-assets/kestrel-0.10.0.tar.gz"
+  sha256 "78756bf6de69b92174d5e003084bb0b8e3f495d794ad86c12f2b6be8c82a961d"
+  license "BSD-3-Clause"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    man1.install "docs/man/kestrel.1"
+    bash_completion.install "completion/kestrel.bash-completion"
+  end
+
+  test do
+    assert_equal "v0.10.0", shell_output("#{bin}/kestrel --version").chomp
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Kestrel Homebrew Tap
+
+Install kestrel in homebrew
+
+## Installation
+
+```
+brew tap finfet/kestrel-cli
+brew install kestrel-cli
+```
