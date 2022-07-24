543bbbfb5c745c6498d201b02ce4a14140bcf6c8
Kyle Schreiber <kyle@80x24.net>
2022-07-23 23:51:04 -0700

    Use resource block for choosing binaries

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -1,26 +1,43 @@
 class KestrelCli < Formula
   desc "File encryption done right"
   homepage "https://getkestrel.com"
-  url "https://storage.googleapis.com/kestrel-assets/kestrel-macos-v0.10.0-all.tar.gz"
-  sha256 "8e638e46c7ee24f7f45919a400dd91cea4608e8e1a1a0d4be8a250fe0234cf0e"
+  url "https://storage.googleapis.com/kestrel-assets/kestrel-macos-v0.10.0-arm64.tar.gz"
+  sha256 "650839679ac2db0777eecff0a5172a6732f87503bfac07fc675d18bebd1b2028"
   license "BSD-3-Clause"
   version "0.10.0"
 
-  def install
-    # Determine which binary to install based on the current architecture
-    arch = `arch`.strip
-    if arch == "x86_64"
-      bin.install "bin/amd64/kestrel"
-    elsif arch == "arm64"
-      bin.install "bin/arm64/kestrel"
-    else
-      raise "Could not find valid architecture"
+  resource "binary" do
+    checksums = {
+      "macos-arm64" => "650839679ac2db0777eecff0a5172a6732f87503bfac07fc675d18bebd1b2028",
+      "macos-amd64" => "37aba44cb587e97d9127f5a7f4bf46db5dfe1ac65758f367437e69187a04905c",
+    }
+
+    arch = "arm64"
+    os = "macos"
+
+    on_intel do
+      arch = "amd64"
     end
-    man1.install "docs/man/kestrel.1"
+
+    on_linux do
+      raise "Homebrew linux is not yet supported. Try the native linux releases."
+    end
+
+    url "https://storage.googleapis.com/kestrel-assets/kestrel-#{os}-v#{version}-#{arch}.tar.gz"
+    sha256 checksums["#{os}-#{arch}"]
+  end
+
+  def install
+    resource("binary").stage {
+      bin.install "kestrel"
+    }
+    man1.install "man/kestrel.1"
     bash_completion.install "completion/kestrel.bash-completion"
+    doc.install "LICENSE.txt"
+    doc.install "THIRD-PARY-LICENSE.txt"
   end
 
   test do
-    assert_equal "v0.10.0", shell_output("#{bin}/kestrel --version").chomp
+    assert_equal "v#{version}" shell_output("#{bin}/kestrel --version").chomp
   end
 end
