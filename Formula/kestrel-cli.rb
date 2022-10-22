47406a19ea4b90d6d3fefde39c296860ebbb362f
Kyle Schreiber <kyle@80x24.net>
2022-10-21 22:32:48 -0700

    Update formula to use binary builds

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -1,23 +1,40 @@
 class KestrelCli < Formula
   desc "File encryption done right"
   homepage "https://getkestrel.com"
-  url "https://getkestrel.com/releases/v0.10.0/kestrel-0.10.0.tar.gz"
-  sha256 "3e5a73c10972eee34253438045f9019d4547445a808a8bb51c8d2e18bc95a843"
   license "BSD-3-Clause"
   version "0.10.0"
 
-  depends_on "rust" => :build
+  if OS.mac?
+    on_intel do
+      url "https://getkestrel.com/releases/v#{version}/kestrel-macos-v#{version}-amd64.tar.gz"
+      sha256 "4820143a9de8e3b450a3a2270fe9cdaa004912307febee8459868e8d2d00ae08"
+    end
+
+    on_arm do
+      url "https://getkestrel.com/releases/v#{version}/kestrel-macos-v#{version}-arm64.tar.gz"
+      sha256 "5deac28cac4e4beebba18ca8869e2e6c48ffffefc057c9e6440238f6cc524769"
+    end
+  elsif OS.linux?
+    on_intel do
+      url "https://getkestrel.com/releases/v#{version}/kestrel-linux-v#{version}-amd64.tar.gz"
+      sha256 "8bd29d970d166a4159d44d4d17935ef6394721787e838a1eb02e65a072b341bb"
+    end
+
+    on_arm do
+      url "https://getkestrel.com/releases/v#{version}/kestrel-linux-v#{version}-arm64.tar.gz"
+      sha256 "38e189e8a4b1ce5f1c5c3cbd9b53210a2bfb5a561714da938962b7880a645732"
+    end
+  end
 
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/kestrel"
-    man1.install "docs/man/kestrel.1"
+    bin.install "kestrel"
+    man1.install "man/kestrel.1"
     bash_completion.install "completion/kestrel.bash-completion"
     doc.install "LICENSE.txt"
     doc.install "THIRD-PARTY-LICENSE.txt"
   end
 
   test do
-    assert_equal "v0.10.0", shell_output("#{bin}/kestrel --version").chomp
+    assert_equal "v#{version}", shell_output("#{bin}/kestrel --version").chomp
   end
 end
