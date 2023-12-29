422c1ce68f84638f821c4472353ba56b25834bf8
Kyle Schreiber <kyle@80x24.net>
2023-12-29 00:56:26 -0800

    Add bottle url to formula

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -6,6 +6,12 @@ class KestrelCli < Formula
   url "https://getkestrel.com/releases/v#{version}/kestrel-#{version}.tar.gz"
   sha256 "83fc5dc6a487cefc1ef3c9d07b60193c1292b5988afe05d9ed11c98e68a871da"
 
+  bottle do
+    root_url "https://bottles.getkestrel.com"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "6dfdb9b5bf14e8df3a76642c280eccab2f18a6a07879bb0d32a941c86b234bf0"
+  end
+
   depends_on "rust" => :build
 
   def install
