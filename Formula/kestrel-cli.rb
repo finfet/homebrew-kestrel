f4405659a2bf9751c6ef968c6526eeac93f3e558
Kyle Schreiber <kyle@80x24.net>
2023-12-29 03:09:33 -0800

    Remove bottle url

--- Formula/kestrel-cli.rb
+++ Formula/kestrel-cli.rb
@@ -5,12 +5,6 @@ class KestrelCli < Formula
   sha256 "83fc5dc6a487cefc1ef3c9d07b60193c1292b5988afe05d9ed11c98e68a871da"
   license "BSD-3-Clause"
 
-  bottle do
-    root_url "https://bottles.getkestrel.com"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "6dfdb9b5bf14e8df3a76642c280eccab2f18a6a07879bb0d32a941c86b234bf0"
-  end
-
   depends_on "rust" => :build
 
   def install
